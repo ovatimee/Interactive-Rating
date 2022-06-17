0a423bbca9ff2e2f1b0d130e8cbf3b1f10af5605
jason606 <stonegee606@yahoo.com>
2022-06-17 11:08:13 +0000

    Initial Commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const container = document.querySelector(".rating-container");
+const submit = document.querySelector(".submit");
+
+submit.addEventListener("click", () => {
+  console.log("go");
+});
